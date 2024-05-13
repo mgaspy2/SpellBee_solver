69b22ce98e78f8916039ac46f5ae21c31de3b601
Michal Gasper <104723740+mgaspy2@users.noreply.github.com>
2024-05-13 03:47:28 +0200

    Code formatting, improved time display

--- SpellBee_solver.py
+++ SpellBee_solver.py
@@ -1,5 +1,6 @@
-# SpellBee.com solver (can be used for other word games as well - suitable dictionary required [AllWords.txt - 60k words, Answers.txt - 39k words])
-# Tested on AMD Ryzen 7 5800H
+# SpellBee.com solver (can be used for other word games as well - suitable dictionary required for validation [AllWords.txt - 60k words, Answers.txt - 39k words])
+# Tested with minimal word length of 4 on:
+# AMD Ryzen 7 5800H
 # 8 cores, 16 threads at 3.2GHz
 
 import csv
@@ -7,8 +8,9 @@ import multiprocessing
 from functools import partial
 from tqdm import tqdm
 
-def binary_search(word, file_path='Answers.txt'):
-    with open(file_path, 'r') as file:
+
+def binary_search(word, file_path="Answers.txt"):
+    with open(file_path, "r") as file:
         lines = file.readlines()
         low = 0
         high = len(lines) - 1
@@ -26,10 +28,12 @@ def binary_search(word, file_path='Answers.txt'):
 
         return -1
 
+
 class TreeNode:
     def __init__(self, data):
         self.data = data
         self.children = []
+
 
 class Words:
     def __init__(self, letters, required_letter, min_length, max_length):
@@ -42,7 +46,9 @@ class Words:
         self.dfs_duration = 0
 
         if self.min_length > self.max_length:
-            raise ValueError("Minimum word length cannot be greater than maximum word length.")
+            raise ValueError(
+                "Minimum word length cannot be greater than maximum word length."
+            )
 
     @staticmethod
     def count_nodes(node):
@@ -60,10 +66,10 @@ class Words:
     def _build_tree_recursive(self, node, depth):
         if depth >= self.max_length:
             return
-        
+
         for letter in self.letters:
             node.children.append(TreeNode(letter))
-        
+
         for child in node.children:
             self._build_tree_recursive(child, depth + 1)
 
@@ -80,37 +86,54 @@ class Words:
                 print("  " * level + "├─" + node.data)
                 for child in node.children:
                     self.display_tree(child, level + 1)
-                    
+
     def save_to_csv(self, file_path=None):
         if file_path is None:
-            file_name = f"{''.join(self.letters)}-{self.required_letter}-{self.min_length}-{self.max_length}-{self.dfs_duration}min.csv"
-            file_path = file_name
-        with open(file_path, 'w', newline='') as csvfile:
-            fieldnames = ['Word', 'Level', 'Index']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            duration = self.dfs_duration
+            if duration > 60:
+                duration = f"{duration/60:.2f}min"
+            elif duration > 1:
+                duration = f"{duration:.2f}sec"
+            elif duration > 0.001:
+                duration = f"{duration*1000:.2f}ms"
+            else:
+                duration = f"{duration*1000000:.2f}µs"
 
+            file_name = f"{''.join(self.letters)}-{self.required_letter}-tree-{self.min_length}-{self.max_length}-{duration}.csv"
+            file_path = file_name
+
+        with open(file_path, "w", newline="") as csvfile:
+            fieldnames = ["Word", "Level", "Index"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             for word, level, index in self.found_words:
-                writer.writerow({'Word': word, 'Level': level, 'Index': index})
+                writer.writerow({"Word": word, "Level": level, "Index": index})
 
         print(f"\nCSV file saved as: {file_path}\n")
-        
+
     def dfs(self, show=False):
         nodes_count = self.count_nodes(self.root)
-        
+
         with tqdm(total=nodes_count, desc="DFS Progress") as pbar:
-            self._dfs_recursive(self.root, '', 0, pbar)
-            
-        self.dfs_duration = round(pbar.format_dict['elapsed'] / 60)
+            self._dfs_recursive(self.root, "", 0, pbar)
+
+        self.dfs_duration = pbar.format_dict["elapsed"]
 
         if show:
-            print("\nFound {} words:\n".format(len(self.found_words)))
+            print(f"\nFound {len(self.found_words)} words:\n")
             for word, level, index in self.found_words:
                 print(f"Word: {word}, Level: {level}, Index: {index+1}")
         else:
-            print("\nFound {} words".format(len(self.found_words)))
-            
-        print(f"\nDFS Duration: {int(pbar.format_dict['elapsed'] / 60)}m {int(pbar.format_dict['elapsed'] % 60)}s")
+            print(f"\nFound {len(self.found_words)} words")
+
+        if self.dfs_duration > 60:
+            print(f"\nDFS Duration: {self.dfs_duration/60:.2f} minutes")
+        elif self.dfs_duration > 1:
+            print(f"\nDFS Duration: {self.dfs_duration:.2f} seconds")
+        elif self.dfs_duration > 0.001:
+            print(f"\nDFS Duration: {self.dfs_duration*1000:.2f} milliseconds")
+        else:
+            print(f"\nDFS Duration: {self.dfs_duration*1000000:.2f} microseconds")
 
     def _dfs_recursive(self, node, word_memory, level, pbar=None):
         if node is None:
@@ -119,21 +142,26 @@ class Words:
         if level > 0:
             word_memory += node.data
 
-        if level >= 3 and word_memory[-1] == word_memory[-2] == word_memory[-3]:    # 1st optimization: if there are 3 consecutive same letters in the word then skip the word - ToDO: add more optimizations if possible
+        if (
+            level >= 3 and word_memory[-1] == word_memory[-2] == word_memory[-3]
+        ):  # 1st optimization: if there are 3 consecutive same letters in the word then skip the word - ToDO: add more optimizations if possible
             if pbar is not None:
                 pbar.update(self.count_nodes(node))
             return
-        elif self.required_letter in word_memory and self.min_length <= level <= self.max_length:
+        elif (
+            self.required_letter in word_memory
+            and self.min_length <= level <= self.max_length
+        ):
             index = binary_search(word_memory)
             if index != -1:
                 self.found_words.append((word_memory, level, index))
 
         if pbar is not None:
             pbar.update(1)
-        
+
         for child in node.children:
             self._dfs_recursive(child, word_memory, level + 1, pbar)
-    
+
     def dfs_parallel(self, show=False):
         processes_num = len(self.root.children)
         cores_num = multiprocessing.cpu_count()
@@ -143,62 +171,77 @@ class Words:
         pool = multiprocessing.Pool(processes_num)
 
         partial_dfs_recursive = partial(
-            _dfs_recursive_parallel, 
-            word_memory='', 
-            level=1, 
-            required_letter=self.required_letter, 
-            min_length=self.min_length, 
-            max_length=self.max_length
+            _dfs_recursive_parallel,
+            word_memory="",
+            level=1,
+            required_letter=self.required_letter,
+            min_length=self.min_length,
+            max_length=self.max_length,
         )
 
-        with tqdm(total=processes_num, desc="Parallel DFS Progress") as pbar:   # progress bar updates after each process is completed - ToDO: update progress bar after each node is completed
+        with tqdm(
+            total=processes_num, desc="Parallel DFS Progress"
+        ) as pbar:  # progress bar updates after each process is completed - ToDO: update progress bar after each node is completed
             results = []
             for result in pool.imap(partial_dfs_recursive, self.root.children):
                 results.append(result)
                 pbar.update(1)
 
-        self.dfs_duration = round(pbar.format_dict['elapsed'] / 60)
-        
+        self.dfs_duration = pbar.format_dict["elapsed"]
+
         for found_words in results:
             self.found_words.extend(found_words)
 
         pool.close()
         pool.join()
-        
+
         if show:
-            print("\nFound {} words:\n".format(len(self.found_words)))
+            print(f"\nFound {len(self.found_words)} words:\n")
             for word, level, index in self.found_words:
                 print(f"Word: {word}, Level: {level}, Index: {index+1}")
         else:
-            print("\nFound {} words".format(len(self.found_words)))
-            
-        print(f"\nParallel DFS Duration: {int(pbar.format_dict['elapsed'] / 60)}m {int(pbar.format_dict['elapsed'] % 60)}s")
-            
-def _dfs_recursive_parallel(node, word_memory, level, required_letter, min_length, max_length):
+            print(f"\nFound {len(self.found_words)} words")
+
+        if self.dfs_duration > 60:
+            print(f"\nParallel DFS Duration: {self.dfs_duration/60:.2f} minutes")
+        elif self.dfs_duration > 1:
+            print(f"\nParallel DFS Duration: {self.dfs_duration:.2f} seconds")
+        elif self.dfs_duration > 0.001:
+            print(f"\nParallel DFS Duration: {self.dfs_duration*1000:.2f} milliseconds")
+        else:
+            print(
+                f"\nParallel DFS Duration: {self.dfs_duration*1000000:.2f} microseconds"
+            )
+
+
+def _dfs_recursive_parallel(
+    node, word_memory, level, required_letter, min_length, max_length
+):
     memory_tree = Words([], required_letter, min_length, max_length)
     memory_tree._dfs_recursive(node, word_memory, level)
 
     return memory_tree.found_words
 
+
 if __name__ == "__main__":
     # from letters import letters, letter
     # input_letters = letters[0]
     # required_letter = letter[0]
-    
+
     # Input letters, required letter, minimum word length, and maximum word length
-    input_letters = ['d', 'o', 'r', 's', 'e', 't', 'y']  # 7 letters is default
-    required_letter = 'y'  # Letter that must be included in the word
+    input_letters = ["d", "o", "r", "s", "e", "t", "y"]  # 7 letters is default
+    required_letter = "y"  # Letter that must be included in the word
     min_word_length = 4  # Minimum word length, default is 4
     max_word_length = 5  # Maximum word length
 
     word_tree = Words(input_letters, required_letter, min_word_length, max_word_length)
     word_tree.build_tree()
     # word_tree.display_tree(word_tree.root)
-    # word_tree.dfs()             # Maximum word length: 5 - 27 seconds (19608 nodes), 6 - 4 minutes (137257 nodes), 7 - 29 minutes (960800 nodes) 
-    word_tree.dfs_parallel()    # Maximum word length: 5 - 13 seconds (19608 nodes), 6 - 2 minutes (137257 nodes), 7 - 20 minutes (960800 nodes) [parallelization on 7 cores - each letter is a separate process]
+    # word_tree.dfs()  # Maximum word length: 5 - 27 seconds (19608 nodes), 6 - 4 minutes (137257 nodes), 7 - 29 minutes (960800 nodes)
+    word_tree.dfs_parallel()  # Maximum word length: 5 - 13 seconds (19608 nodes), 6 - 2 minutes (137257 nodes), 7 - 20 minutes (960800 nodes) [parallelization on 7 cores - each letter is a separate process]
     word_tree.save_to_csv()
-    
+
     # Times before optimization
-    
+
     # optimization: if level >= 3 and word_memory[-1] == word_memory[-2] == word_memory[-3] and dictionary change to Answers.txt (dictionary change is only SpellBee optimization)
     # dfs() - max_word_length: 5 takes - 1 minute, 6 - 7 minutes, 7 - 50 minutes
